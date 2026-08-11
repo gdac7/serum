7c762de10eab05dda1bd631b77cdb499d0b8245a
gdac7 <gabrieldalcs@gmail.com>
2026-08-11 20:40:41 -0300

    chore: add comments in setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -1,3 +1,4 @@
-docker run -d --name pgvector -e POSTGRES_PASSWORD=gdac1908 -p 5432:5432 pgvector/pgvector:pg17
+# This needs to be runned in the same host of the service container (for while)
+# docker run -p 5432:5432 -d --name pgvector -e POSTGRES_PASSWORD=gdac1908 -p 5432:5432 pgvector/pgvector:pg17
 export DATABASE_URL="postgresql://postgres:gdac1908@localhost:5432/postgres"
 pip install "psycopg[binary,pool]" pgvector
