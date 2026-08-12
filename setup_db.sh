66fc0b0f20b973e0f5f468dc9073c0359775c667
gdac7 <gabrieldalcs@gmail.com>
2026-08-11 22:03:43 -0300

    chore: change format of docker run instruction in setup_db.sh

--- setup_db.sh
+++ setup_db.sh
@@ -1,4 +1,4 @@
 # This needs to be runned in the same host of the service container (for while)
-# docker run -p 5432:5432 -d --name pgvector -e POSTGRES_PASSWORD=gdac1908 -p 5432:5432 pgvector/pgvector:pg17
+# docker run -d --name pgvector -e POSTGRES_PASSWORD=gdac1908 -p 4534:5432 pgvector/pgvector:pg17export DATABASE_URL="postgresql://postgres:gdac1908@localhost:5432/postgres"
 export DATABASE_URL="postgresql://postgres:gdac1908@localhost:5432/postgres"
 pip install "psycopg[binary,pool]" pgvector
